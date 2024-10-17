48e848125b7c0491cb339a76a3cc0a3133645c09
0xtbug <tubagus.nabil19@gmail.com>
2024-10-18 02:47:53 +0700

    feat: add user endpoints

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const userRoutes = require("./src/routes/userRoutes");
 const app = express();
 
 app.use(bodyParser.json());
 
-// TODO: add routes for user endpoints
+// User Routes
+app.use("/api/v1/users", userRoutes);
 
 // TODO: Add routes for Accounts endpoints
 
